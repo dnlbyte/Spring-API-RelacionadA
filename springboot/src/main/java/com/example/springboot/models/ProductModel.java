ecb17cd2b9408a35eb869d615f07a1fa944f58f8
Daniel Silva De Lima <daniel_lma@hotmail.com>
2023-09-18 21:50:27 -0300

    feat: Modela a entidade de ProductModel e implementa a relação manyToOne com a Entidade PersonModel

--- springboot/src/main/java/com/example/springboot/models/ProductModel.java
+++ springboot/src/main/java/com/example/springboot/models/ProductModel.java
@@ -1,10 +1,33 @@
 package com.example.springboot.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.UUID;
 
+@Entity
+@Data
+@Table(name = "TB_PRODUCT")
 public class ProductModel {
 
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", length = 100, nullable = false, unique = true)
+    private String name;
 
 
+    @Column(name = "description", length = 200, nullable = false)
+    private String description;
 
+    @Column(name = "unitprice", nullable = false, precision = 10, scale = 2)
+    private BigDecimal unitPrice;
+
+    @Column(name = "quantity", length = 6, nullable = false)
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "id_person")
+    private PersonModel  person;
 }
