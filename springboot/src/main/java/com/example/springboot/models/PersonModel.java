8b5ec6d775a853f990e676480804d302fc67943e
Daniel Silva De Lima <daniel_lma@hotmail.com>
2023-09-18 21:51:30 -0300

    feat: Modela a entidade de PersonModel e implementa o mapeamento de relação oneToOne

--- springboot/src/main/java/com/example/springboot/models/PersonModel.java
+++ springboot/src/main/java/com/example/springboot/models/PersonModel.java
@@ -1,7 +1,27 @@
 package com.example.springboot.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
 
+import java.util.UUID;
 
+@Entity
+@Data
+@Table(name = "TB_PERSON")
 public class PersonModel {
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+
+    @Column(name = "cpf", length = 11, nullable = false, unique = true)
+    private String cpf;
+
+    @Column(name = "isActive", nullable = false)
+    private Boolean isActive;
+
+    @OneToOne(mappedBy = "person")
+    private UserModel user;
 }
