96e4e9f3f05b6cf56712fa7fe696b540a8e60c08
Daniel Silva de lima <75702871+dnlbyte@users.noreply.github.com>
2024-04-12 12:28:15 -0300

    Update PersonModel.java
    Implementação do relacionamento de ManyToOne para as tabelas person e product

--- springboot/src/main/java/com/example/springboot/models/PersonModel.java
+++ springboot/src/main/java/com/example/springboot/models/PersonModel.java
@@ -1,7 +1,29 @@
-package com.example.springboot.models;
+package com.Danielsilvadelima.CrudProduto.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
+@Entity
+@Data
+@Table(name="TB_PERSON")
 public class PersonModel {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private UUID id;
 
+    @Column(name = "name", nullable = false, unique = false)
+    private String name;
+
+    @Column(name = "cpf", nullable = false, unique = true)
+    private String cpf;
+
+    @Column(name = "isActive", nullable = false)
+    private Boolean isActive;
+
+    @OneToMany(mappedBy = "person")
+    private Set<ProductModel> products = new HashSet<>();
 }
