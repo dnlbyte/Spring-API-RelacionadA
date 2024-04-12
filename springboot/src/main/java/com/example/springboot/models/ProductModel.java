e0485d29321f5cfa005fc3a68cf1e09d47423455
Daniel Silva de lima <75702871+dnlbyte@users.noreply.github.com>
2024-04-12 12:27:31 -0300

    Update ProductModel.java
    Implementação do relacionamentod de ManyToOne para as tabelas person e product

--- springboot/src/main/java/com/example/springboot/models/ProductModel.java
+++ springboot/src/main/java/com/example/springboot/models/ProductModel.java
@@ -1,10 +1,33 @@
-package com.example.springboot.models;
+package com.Danielsilvadelima.CrudProduto.model;
 
+import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+import lombok.Data;
 
+@Entity
+@Data
+@Table(name= "TB_PRODUTO")
 public class ProductModel {
+    // MODELAGEM DO BANCO DE DADOS
+    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name ="name", length = 100, nullable = false, unique = true) 
+    private  String name;
+    
+    @Column(name = "unitPrice", nullable = false, precision = 10, scale = 2)
+    private BigDecimal unitPrice;
+    
+    @Column(name ="stockQuantity", length = 6, nullable = false)
+    private Integer stockQuantity;
+    
+    @Column(name = "isActive", nullable = false)
+    private Boolean isActive;
 
-
-
-
+    @ManyToOne
+    @JoinColumn(name = "person_id", nullable = false)
+    private PersonModel person;
+  
 }
